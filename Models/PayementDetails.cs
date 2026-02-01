97d2b85450ea7af4a71f25f638e724975dc48131
Anshu <ansu18k@gmail.com>
2026-02-01 00:11:56 -0800

    Added model folder and property

---
+++ Models/PayementDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentAPI.Models
+{
+    public class PayementDetails
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        public string CardOwnerName { get; set; }
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string SecurityCode { get; set; }
+
+
+    }
+}
